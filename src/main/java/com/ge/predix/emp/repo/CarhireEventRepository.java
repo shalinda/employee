d9a225887e5afd2b4906d43a9d47c483ffaa7d9c
shalinda <shalinda@gmail.com>
2017-03-07 05:17:55 -0500

    Carhire Event lookup added

--- src/main/java/com/ge/predix/emp/repo/CarhireEventRepository.java
+++ src/main/java/com/ge/predix/emp/repo/CarhireEventRepository.java
@@ -2,10 +2,13 @@ package com.ge.predix.emp.repo;
 
 import com.ge.predix.emp.domain.CarhireEvent;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import java.util.List;
 
 /**
  * Created by shalindaranasinghe on 2/21/17.
  */
 public interface CarhireEventRepository extends CrudRepository<CarhireEvent, Integer> {
 
+    List<CarhireEvent> findByEquipInitial(@Param("equipInitial") String equipInitial);
 }
